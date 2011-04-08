33b005ec53d2c7e8940b41e04b05c5faed48418b
Dario Strbenac <d.strbenac@garvan.org.au>
2011-04-08 17:44:14 +1000

    genQC now works on SequenceQCSet or character vector of paths.

--- pkg/Repitools/R/genQC.R
+++ pkg/Repitools/R/genQC.R
@@ -1,4 +1,4 @@
-setGeneric("genQC", signature = c("paths", "expt"), function(paths, expt)
+setGeneric("genQC", signature = c("qc.data", "expt"), function(qc.data, expt)
                                               {standardGeneric("genQC")})
 
 .plotFreqs <- function(freqs, l.names, aligned)
@@ -15,43 +15,41 @@ setGeneric("genQC", signature = c("paths", "expt"), function(paths, expt)
     }, freqs, l.names))
 }
 
-setMethod("genQC", "character", 
-    function(paths, expt)
+setMethod("genQC", "SequenceQCSet", 
+    function(qc.data, expt)
 {
-    if(is.null(names(paths)))
-        l.names <- paste("Lane", 1:length(paths))
+    if(is.null(names(qc.data)))
+        l.names <- paste("Lane", 1:length(qc.data))
     else
-        l.names <- names(paths)
+        l.names <- names(qc.data)
 
-    qual <- lapply(paths, function(x) get(load(x)))
-
-    n.reads <- lapply(qual, function(x)
+    n.reads <- lapply(qc.data, function(x)
                    as.integer(Basic_Statistics(x, "Unaligned")[3, 2]))
-    n.aln <- lapply(qual, function(x)
+    n.aln <- lapply(qc.data, function(x)
                  as.integer(Basic_Statistics(x, "Aligned")[3, 2]))
 
-    avg.q <- sapply(qual, function(x)
+    avg.q <- sapply(qc.data, function(x)
                  Per_base_sequence_quality(x, "Unaligned")[, 2])
 
     base.freq <- mapply(function(x, y)
     {
         cbind(Per_base_sequence_content(x, "Unaligned")[, 2:5] / y,
               Per_base_N_content(x, "Unaligned")[, 2] / 100)
-    }, qual, n.reads, SIMPLIFY = FALSE)
+    }, qc.data, n.reads, SIMPLIFY = FALSE)
 
     base.freq.aln <- mapply(function(x, y)
     {
         cbind(Per_base_sequence_content(x, "Aligned")[, 2:5] / y,
               Per_base_N_content(x, "Aligned")[, 2] / 100)
-    }, qual, n.aln, SIMPLIFY = FALSE)
+    }, qc.data, n.aln, SIMPLIFY = FALSE)
 
-    mm.freqs <- lapply(qual, function(x)
+    mm.freqs <- lapply(qc.data, function(x)
     {
 	mism <- Mismatches(x)[, 2:17]
         t(apply(mism, 1, function(y) y / sum(y)))
     })
 
-    dupls <- lapply(qual, function(x)
+    dupls <- lapply(qc.data, function(x)
     {
 	dupl <- Sequence_Duplication_Levels(x)
 	return(as.numeric(dupl[2:nrow(dupl), 2]))
@@ -90,4 +88,15 @@ setMethod("genQC", "character",
                         xlab = "Occurrences", ylab = "Relative Number", xaxt = 'n')
         axis(1, at = bar.x, labels = c(1:9, "10+"), cex.axis = 0.5)
     }, dupls, l.names))
+
+})
+
+setMethod("genQC", "character", 
+    function(qc.data, expt)
+{
+    qual <- lapply(qc.data, function(x) get(load(x)))
+
+    QCset <- new("SequenceQCSet", qual)
+    names(QCset) <- names(qc.data)
+    genQC(QCset, expt)
 })
